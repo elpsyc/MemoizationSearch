0194240e40b93d72d5013fdf43902bf84c5579f6
12UE <1258432472@qq.com>
2024-03-14 13:04:37 +0800

    更新

--- MemoizationSearch/MemoizationSearch.cpp
+++ MemoizationSearch/MemoizationSearch.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 #include"MemoizationSearch.h"
 //斐波那契数列的缓存搜索
-__int64 Fibonacci(int n) {
+DWORD64 Fibonacci(int n) {
 	if (n <= 1) return n;
 	//利用nonstd::makecached函数生成一个缓存搜索的函数
 	static auto fib = nonstd::makecached(Fibonacci);
 	return fib(n - 1) + fib(n - 2);
 }
 int main(){
-	std::cout << Fibonacci(10);
+	std::cout << Fibonacci(60);
 	return 0;
 }
