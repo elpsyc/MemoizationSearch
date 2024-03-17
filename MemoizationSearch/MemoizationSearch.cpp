3e5b2e4a553cfef14a085c0f1563c1cbbc7446f3
12UE <1258432472@qq.com>
2024-03-17 15:43:36 +0800

    更新

--- MemoizationSearch/MemoizationSearch.cpp
+++ MemoizationSearch/MemoizationSearch.cpp
@@ -11,7 +11,7 @@ int foo1() {
 DWORD64 Fibonacci(int n) {
     if (n <= 1) return n;
     //利用nonstd::makecached函数生成一个缓存搜索的函数
-    auto& fib = nonstd::makecached(Fibonacci);
+    auto& fib = nonstd::makecached(Fibonacci);//一个函数只会生成一个实例
     return fib(n - 1) + fib(n - 2);
 }
 int main() {
